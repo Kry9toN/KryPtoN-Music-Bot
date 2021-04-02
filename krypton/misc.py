3b3dbfcb5350a08460fea31d382d484df808d1d1
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-04-03 01:19:03 +0700

    update help dialog

--- krypton/misc.py
+++ krypton/misc.py
@@ -3,8 +3,13 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 **/help** __To Show This Message.__
 **/skip** __To Skip The Current Playing Music.__
 **/play** __<youtube/saavn/deezer> <Song_Name>__
+**/stop** __To Stop Playing Music.__
 **/join** __To Join A Voice Chat.__
 **/leave** __To Leave A Voice Chat.__
+**/mute** __To Mute A Bot.__
+**/unmute** __To Unmute A Bot.__
+**/volume** __<1-200>__
+**/kill** __To Kill A Sevice Bot.__
 **/donation** __To Give Me A Coffe.__"""
 
 START_TEXT = "__**Hi I'm KryPtoN Music Bot. I will sing for you on Voice Call Telegram, Hit /help for look my features.**__"
