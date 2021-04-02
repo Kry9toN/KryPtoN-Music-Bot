fbe41d2d9386f1d5192eeb05f14eb1f2cbcc36f0
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-04-02 23:00:05 +0700

    This now for heroku for default

--- krypton/configs.py
+++ krypton/configs.py
@@ -1,4 +1,4 @@
-HEROKU = False   # NOTE Make it false if you're not deploying on heroku.
+HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
 
 # NOTE these values are for heroku.
 if HEROKU:
